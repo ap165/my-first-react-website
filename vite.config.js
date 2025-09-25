4183eb044b2db7e72cea8662e417cf1f7e53a7f6
Arijit Paine <arijitpaine647@gmail.com>
2025-09-25 19:11:09 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react({refresh: true})],
-  server: {
-    watch: {
-      usePolling: true
-    }
-  }
+  // server: {
+  //   watch: {
+  //     usePolling: true
+  //   }
+  // }
+  base: process.env.VITE_BASE_PATH || "/my-first-react-website"
 })
